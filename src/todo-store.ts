775fa848ac79d51d19db3ed26d8b8517181568d6
Brad Blanchette <bradblanchette@outlook.com>
2026-04-29 20:14:19 +0100

    Implement add method to TodoStore for adding new todos

--- src/todo-store.ts
+++ src/todo-store.ts
@@ -3,4 +3,11 @@ import { Todo } from './todo';
 export class TodoStore {
     private todos: Todo[] = [];
     private nextId: number = 1;
+    add(text: string): void {
+        this.todos.push({
+            id: this.nextId++,
+            text: text,
+            completed: false
+        });
+    }
 }
