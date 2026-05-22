35405a6a22119c96fdd3233b806056a8a95a0a45
Brad Blanchette <bradblanchette@outlook.com>
2026-05-22 17:36:29 +0100

    Add a method to DomControl for dynamic element creation

--- src/dom.ts
+++ src/dom.ts
@@ -9,6 +9,23 @@ export class DomControl {
         }
         this.container = element;
     }
+    createElement(tag: string, content: string, appendToId: string, id?: string, classList?: string): void {
+        const element = document.createElement(tag);
+        element.textContent = content;
+        if(id) {
+            element.setAttribute('id', id);
+        }
+        if(classList)
+        {
+            element.classList.add(classList);
+        }
+        if(this.domElementExists(appendToId)) {
+            document.getElementById(appendToId)?.appendChild(element);
+        }
+        else {
+            throw new Error(`Element with ID ${appendToId} cannot be found.`);
+        }
+    }
     domElementExists(id: string) {
         const element = document.getElementById(id);
         if(!element) {
