9b44ceec291f29c6fb9f45787f6c0e51458ab8c3
Brad Blanchette <bradblanchette@outlook.com>
2026-05-22 17:32:25 +0100

    Add constructor to DomControl for initializing container element

--- src/dom.ts
+++ src/dom.ts
@@ -1,5 +1,12 @@
 import { TodoStore } from './todo-store';
 
 export class DomControl {
-    
+    private container: HTMLElement;
+    constructor(containerId: string) {
+        const element = document.getElementById(containerId);
+        if(!element) {
+            throw new Error(`Element with ID ${containerId} cannot be found.`);
+        }
+        this.container = element;
+    }
 }
